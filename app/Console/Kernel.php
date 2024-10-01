b5884eeb810157f67a9bfe8e70f7513af5777c3a
VadimKazan4392 <vadim.4392@mail.ru>
2024-10-01 22:09:16 +0300

    by minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command(ParseAds::class)->everyTenMinutes();
+        $schedule->command(ParseAds::class)->everyFiveMinutes();
     }
 
     /**
