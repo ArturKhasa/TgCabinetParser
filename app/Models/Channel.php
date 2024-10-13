9ceae39c9674a4a2efcf53213a082fe75af746e6
VadimKazan4392 <vadim.4392@mail.ru>
2024-10-14 00:53:44 +0300

    parse posts by channel

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Channel extends Model
 {
@@ -16,4 +19,31 @@ class Channel extends Model
         'username',
         'phone_id'
     ];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(ChannelPost::class, 'channel_id', 'id');
+    }
+
+    public function telegramPhone(): BelongsTo
+    {
+        return $this->belongsTo(TelegramPhone::class, 'phone_id', 'id');
+    }
+
+    public function setTelegramPhone(TelegramPhone $phone): void
+    {
+        $this->update(["phone_id" => $phone->id]);
+        $phone->increment("count", 1);
+        if($phone->count >= 100)
+            $phone->update(["open" => false]);
+    }
+
+    public function postStats(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            ChannelPostStat::class, ChannelPost::class,
+            'channel_id', 'post_id', 'id', 'id'
+        );
+    }
 }
+
